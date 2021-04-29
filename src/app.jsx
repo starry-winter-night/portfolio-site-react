920b845beb8cc406692b9add6515b3195ff4a583
smpark <smpark7723@gmail.com>
2021-04-30 07:47:03 +0900

    React : Add aside.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,10 @@
 import './app.module.css';
-import Navbar from './components/navbar';
+import Navbar from './components/site_navbar/navbar';
+import Aside from './components/site_aside/aside';
 import styles from './app.module.css';
 import React, { useRef, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const App = ({ starryNight }) => {
   const canvasRef = useRef();
@@ -14,6 +17,7 @@ const App = ({ starryNight }) => {
     <>
       <canvas ref={canvasRef} className={styles.canvas}></canvas>
       <Navbar />
+      <Aside FontAwesomeIcon={FontAwesomeIcon} faBars={faBars} />
     </>
   );
 };
