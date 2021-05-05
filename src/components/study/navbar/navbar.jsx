bfe71c9b8153e94b1d5e741d64ae80ac2cfabf1a
smpark <smpark7723@gmail.com>
2021-05-05 23:18:56 +0900

    React : Add Study Navbar.jsx

---
+++ src/components/study/navbar/navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from './navbar.module.css';
+import { faArrowLeft, faSearch } from '@fortawesome/free-solid-svg-icons';
+
+const Navbar = ({ FontAwesome }) => {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.backBox}>
+        <FontAwesome className={styles.backIcon} icon={faArrowLeft} />
+      </div>
+      <form className={styles.inputBox}>
+        <input className={styles.searchInput} placeholder="검색"></input>
+        <button className={styles.searchButton}>
+          <FontAwesome className={styles.searchIcon} icon={faSearch} />
+        </button>
+      </form>
+      <ul className={styles.studyList}>
+        <li className={styles.music}>Search</li>
+        <li className={styles.frontEnd}>Develop</li>
+        <li className={styles.etc}>Card</li>
+        <li className={styles.backEnd}>BookMark</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
