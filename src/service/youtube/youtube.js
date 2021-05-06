a5318b697640e32bdd90b52c6129803c82b43a03
smpark <smpark7723@gmail.com>
2021-05-07 08:52:49 +0900

    React : Add Youtube search method

--- src/service/youtube/youtube.js
+++ src/service/youtube/youtube.js
@@ -6,15 +6,27 @@ class Youtube {
       redirect: 'follow',
     };
   }
-  async playList() {
+  async developList() {
+    const id = process.env.REACT_APP_YOUTUBE_MYCHENNEL_ID;
     const response = await fetch(
-      `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails%2Cstatus&playlistId=PLng3cGDHQcFTXjUchGUbih7JuX3ZIp5C6&key=${this.key}`,
+      `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails%2Cstatus&playlistId=${id}&maxResults=25&key=${this.key}`,
       this.getRequestOptions
     );
 
     const result = await response.json();
 
     return result.items;
+  }
+
+  async search(query) {
+    const response = await fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&key=${this.key}`,
+      this.getRequestOptions
+    );
+    
+    const result = await response.json();
+
+    console.log(result);
   }
 }
 
