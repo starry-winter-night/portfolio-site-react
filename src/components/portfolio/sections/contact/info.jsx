246c5c9716f4fb1f1022f18781e95a4fd7f9237b
smpark <smpark7723@gmail.com>
2021-07-07 01:13:10 +0900

    REACT : Add info.jsx Click text copy

--- src/components/portfolio/sections/contact/info.jsx
+++ src/components/portfolio/sections/contact/info.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import styles from './info.module.css';
 const info = ({ info }) => {
+  const onTextClick = (e) => {
+    e.preventDefault();
+
+    const text = e.currentTarget.textContent;
+    const textArea = document.createElement('textarea');
+    document.body.appendChild(textArea);
+    textArea.value = text;
+    textArea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textArea);
+
+    alert("복사 되었습니다.");
+  };
+
   return (
     <div className={styles.content}>
       <h2>{info.title}</h2>
       <a href={info.url} target="noopener">
         {info.icon}
       </a>
-      <p>{info.content}</p>
+      <p className={styles.contentText} onClick={onTextClick}>
+        {info.content}
+      </p>
     </div>
   );
 };
